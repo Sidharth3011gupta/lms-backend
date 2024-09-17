0b853f7ce4f59257e68c9409914ca82587022cf7
Sidharth3011Gupta <sidharthgupta004@gmail.com>
2024-09-17 13:48:39 +0530

    Update

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -23,17 +23,24 @@ const addBook = async (req, res) => {
   }
 };
 
-const updateBookBorrowStatus = async (req, res) => {
+const updateBookBorrowStatus=async (req, res) => {
+  const { id } = req.params;
+  const { isborrowed } = req.body;
   try {
-    const book = await Book.findById(req.params.id);
-    if (!book) {
-      return res.status(404).json({ message: 'Book not found' });
-    }
-    book.isBorrowed = !book.isBorrowed;
-    await book.save();
-    res.json(book);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+      const updatedBook = await Book.findByIdAndUpdate(
+          id,
+          { isborrowed },
+          { new: true }
+      );
+
+      if (!updatedBook) {
+          return res.status(404).json({ message: 'Book not found' });
+      }
+
+      res.status(200).json(updatedBook);
+  } catch (error) {
+      console.error('Error updating book:', error);
+      res.status(500).json({ message: 'Server error while updating book' });
   }
 };
 
